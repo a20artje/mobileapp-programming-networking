093dd27c1b887a83592ff137de3b3e6fefc4ce15
a20artje <coolvids.bruh@gmail.com>
2024-04-22 13:26:16 +0200

    created mountain class

--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -1,4 +1,15 @@
 package com.example.networking;
 
 public class Mountain {
+
+    private String name;
+    private String location;
+    private int height;
+
+    public Mountain(String name, String location, int height){
+        this.name = name;
+        this.location = location;
+        this.height = height;
+    }
+
 }
